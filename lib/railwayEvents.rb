262475bb6606049075210bccf7c871873489198c
tomcha <tomcha@tomcha.net>
2016-03-18 12:16:59 +0900

    情報種別の振り分けを実装

--- lib/railwayEvents.rb
+++ lib/railwayEvents.rb
@@ -5,8 +5,14 @@ require 'railwayEvents/version'
 require 'railwayEvents/rssMaker'
 module RailwayEvents
   class Application < Sinatra::Base
-    get '/' do
+    get '/yard.xml' do
       rss = RssMaker::Feeds.new(URI.encode("https://www.tetsudo.com/event/category/車両基地/"))
+      content_type "application/xml"
+      rss.makeFeeds
+    end
+
+    get '/sptrain.xml' do
+      rss = RssMaker::Feeds.new(URI.encode("https://www.tetsudo.com/event/category/臨時列車/"))
       content_type "application/xml"
       rss.makeFeeds
     end
